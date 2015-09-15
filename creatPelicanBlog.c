1ad7791fc098a4a9fbbe38ec72683650ec379c14
解志达 <eva330680229@gmail.com>
2015-09-15 21:56:36 +0800

    完成基本功能
    完成基本功能

--- creatPelicanBlog.c
+++ creatPelicanBlog.c
@@ -45,33 +45,67 @@ int main()
 void creatBlog(int blog_class,int blog_num)
 {
     char bclass;
-    char title[300],category[50],Tags[20][50];
+    char name[15];
+    char title[300],category[50],Tags[20][50],temp[2];
     int i,j;
     SYSTEMTIME t;
+    FILE * fp;
     switch(blog_class)
     {
-        case 1: bclass='t';strcpy(category,"Tech");break;
-        case 2: bclass='m';strcpy(category,"Motion");break;
-        case 3: bclass='o';strcpy(category,"Others");break;
+    case 1:
+        bclass='t';
+        strcpy(category,"Tech");
+        strcpy(name,"t");
+        break;
+    case 2:
+        bclass='m';
+        strcpy(category,"Motion");
+        strcpy(name,"m");
+        break;
+    case 3:
+        bclass='o';
+        strcpy(category,"Others");
+        strcpy(name,"o");
+        break;
     }
-    printf("it will creat %d blog\n and class is %c",blog_num,bclass);
+    for(i=1000; i!=0; i/=10)//循环将数字变成字符串储存进name后
+    {
+        j=blog_num/i;
+        temp[0]='0'+j;
+        temp[1]='\0';
+        strcat(name,temp);
+    }
+    strcat(name,".md");//填补后缀
     printf("请输入你要键入的文章标题\n");
+    getchar();
     gets(title);
     system("cls");
-    printf("请输入该文章的Tag(输入'0'表示输入结束)");
-    do{
-        gets(Tags[i]);
-        i++;
-    }while(Tags[i][0]!='0'&&Tags[i][1]!='\0');
-    printf("Title:%s\n",title);
-    GetLocalTime(&t);
-    printf("Date:%4d-%2d-%02d %d:%d\n",t.wYear,t.wMonth,t.wDay,t.wHour,t.wHour);
-    printf("Category:%s\n",category);
-    printf("Tags:");
+    for(i=0;Tags[i-1][0]!='0'||Tags[i-1][1]!='\0';i++)
+    {
+        system("cls");
+        printf("请输入该文章的第%d条Tag(输入'0'表示输入结束):\n",i+1);
+        scanf("%s",Tags[i]);
+        system("cls");
+    }
 
-    for(i=0;strcmp(Tags[i],"0");i++)
-        printf("%s,",Tags[i]);
-    printf("\n");
-
-    printf("all done\n");
+    fp=fopen(name,"w+");//开始创建文件
+    if(fp==NULL)
+    {
+        printf("错误，文件创建失败\n");
+        exit(0);
+    }
+    GetLocalTime(&t);//获取系统时间 开始写入
+    fprintf(fp,"Title:%s\n",title);
+    fprintf(fp,"Date:%4d-%02d-%02d %d:%02d:%02d\n",t.wYear,t.wMonth,t.wDay,t.wHour,t.wHour,t.wSecond);
+    fprintf(fp,"Category:%s\n",category);
+    fprintf(fp,"Tags:");
+    for(i=0; strcmp(Tags[i],"0"); i++)//循环写入tags
+        fprintf(fp,"%s,",Tags[i]);
+    fprintf(fp,"\n");
+    fprintf(fp,"status: draft\n");
+    fprintf(fp,"\n此处开始写正文，完成后请不要忘记将status:draft 删除。");
+    fprintf(fp,"all done");
+    printf("成功");
+    fflush(fp);
+    fclose(fp);
 }
